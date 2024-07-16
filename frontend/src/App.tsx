4569a793f6af17f6d59c88a45933c2647c7891c2
acegi <acegiansal@gmail.com>
2024-07-16 18:18:52 -0400

    minimal callout is complete

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,16 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
-import ShadowCard from './components/ShadowCard'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import "./App.css";
+import ShadowCard from "./components/ShadowCard";
+import MinimalCallout from "./components/MinCalloutComp/MinimalCallout";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
-    <ShadowCard shadowGap={20} maxWidth={300}>Hello Again</ShadowCard>
+      <MinimalCallout type="right" spacing={3}>
+        Hello, this is a test
+      </MinimalCallout>
+
+      <ShadowCard shadowGap={20} maxWidth={300}>
+        Hello Again
+      </ShadowCard>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- frontend/src/components/MinCalloutComp/MinimalCallout.tsx
+++ frontend/src/components/MinCalloutComp/MinimalCallout.tsx
@@ -3,13 +3,37 @@ import "./MinCallout.css";
 
 interface MinCalloutProps {
   children?: ReactNode;
-  type: "default" | "left" | "right";
+  type?: "default" | "left" | "right";
+  spacing?: number;
+  colour?:
+    | "default"
+    | "primary"
+    | "success"
+    | "danger"
+    | "warning"
+    | "info"
+    | "bdc";
 }
 
-const MinimalCallout = ({ children, type = "default" }: MinCalloutProps) => {
+const MinimalCallout = ({
+  children,
+  type = "default",
+  colour = "default",
+  spacing = 2,
+}: MinCalloutProps) => {
+  let calloutClass = "min-callout ";
+  if (type != "default") {
+    calloutClass += type + "-min-callout";
+  }
+
+  calloutClass += " callout-" + colour;
+
+  //Spacing
+  calloutClass += " mx-" + spacing;
+
   return (
     <>
-      <div></div>
+      <div className={calloutClass}>{children}</div>
     </>
   );
 };
